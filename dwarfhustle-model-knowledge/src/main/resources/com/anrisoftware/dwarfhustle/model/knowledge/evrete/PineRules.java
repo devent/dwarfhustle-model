cbadd588f5fba0273d361779fcbc2fc214ed6dfc
Erwin Müller <erwin@muellerpublic.de>
2024-10-30 20:57:18 +0100

    Add rules

--- dwarfhustle-model-knowledge/src/main/resources/com/anrisoftware/dwarfhustle/model/knowledge/evrete/PineRules.java
+++ dwarfhustle-model-knowledge/src/main/resources/com/anrisoftware/dwarfhustle/model/knowledge/evrete/PineRules.java
@@ -44,7 +44,7 @@ public class PineRules extends AbstractTerrainRules {
 
     @Rule(salience = 1)
     @Where(value = { "$f.v.growth < 0.1",
-            "$f.z > 0 && $f.x == $f.v.pos.x && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z - 1 && $f.isProp(EMPTY.flag)" })
+            "$f.z > 0 && $f.isProp(EMPTY.flag) && $f.x == $f.v.pos.x && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z - 1" })
     public void step_1_leaf_up_pos(VegetationBlockFact $f, RhsContext ctx) {
         System.out.printf("PineRules.step_1_leaf_up_pos(%d, %d, %d)\n", $f.x, $f.y, $f.z); // TODO
         if ($f.getObject($f.x, $f.y, $f.z + 1) == $f.objects.get(OBJECT_TREE_BRANCH_NAME.hash)) {
@@ -52,4 +52,45 @@ public class PineRules extends AbstractTerrainRules {
             $f.setProp(FILLED.flag);
         }
     }
+
+    @Rule(salience = 1)
+    @Where(value = { "$f.v.growth < 0.1",
+            "$f.x > 0 && $f.isProp(EMPTY.flag) && $f.x == $f.v.pos.x - 1 && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z" })
+    public void step_1_leaf_west_pos(VegetationBlockFact $f, RhsContext ctx) {
+        System.out.printf("PineRules.step_1_leaf_west_pos(%d, %d, %d)\n", $f.x, $f.y, $f.z); // TODO
+        if ($f.getObject($f.x + 1, $f.y, $f.z) == $f.objects.get(OBJECT_TREE_BRANCH_NAME.hash)) {
+            $f.setObject(objects.get(OBJECT_TREE_LEAF_NAME.hash));
+            $f.setProp(FILLED.flag);
+        }
+    }
+
+    @Rule(salience = 1)
+    @Where(value = { "$f.v.growth < 0.1",
+            "$f.x < $f.w && $f.isProp(EMPTY.flag) && $f.x == $f.v.pos.x + 1 && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z" })
+    public void step_1_leaf_east_pos(VegetationBlockFact $f, RhsContext ctx) {
+        System.out.printf("PineRules.step_1_leaf_east_pos(%d, %d, %d)\n", $f.x, $f.y, $f.z); // TODO
+        if ($f.getObject($f.x - 1, $f.y, $f.z) == $f.objects.get(OBJECT_TREE_BRANCH_NAME.hash)) {
+            $f.setObject(objects.get(OBJECT_TREE_LEAF_NAME.hash));
+            $f.setProp(FILLED.flag);
+        }
+    }
+
+    @Rule(salience = 1)
+    @Where(value = { "$f.v.growth < 0.1",
+            "$f.z < $f.d && $f.isProp(FILLED.flag) && $f.x == $f.v.pos.x && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z + 1" })
+    public void step_1_root_down_pos(VegetationBlockFact $f, RhsContext ctx) {
+        System.out.printf("PineRules.step_1_root_down_pos(%d, %d, %d)\n", $f.x, $f.y, $f.z); // TODO
+        if ($f.getObject($f.x, $f.y, $f.z - 1) == $f.objects.get(OBJECT_TREE_BRANCH_NAME.hash)) {
+            $f.setObject(objects.get(OBJECT_TREE_ROOT_NAME.hash));
+            $f.setProp(FILLED.flag);
+        }
+    }
+
+    @Rule(salience = -1)
+    @Where(value = { "$f.x == $f.v.pos.x && $f.y == $f.v.pos.y && $f.z == $f.v.pos.z" })
+    public void step_1_increase_grow(VegetationBlockFact $f, RhsContext ctx) {
+        System.out.printf("PineRules.step_1_increase_grow(%d, %d, %d)\n", $f.x, $f.y, $f.z); // TODO
+        $f.v.growth += 0.1f;
+    }
+
 }
